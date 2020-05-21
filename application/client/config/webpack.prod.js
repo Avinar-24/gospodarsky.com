9828270f378bf69dcdbf8784bce231965c70fbcd
Avinar-24 <artyom_gospodarsky@epam.com>
2020-05-20 23:32:59 -0400

    Organize file structure

--- application/client/config/webpack.prod.js
+++ application/client/config/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "main.js",
-    path: path.resolve(`${__dirname}/..`, "dist"),
+    path: path.resolve(`${__dirname}/../..`, "dist"),
   },
   plugins: [new CleanWebpackPlugin(), ...common.plugins],
 });
